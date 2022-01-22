b4255c62093136ea28f5a01e10cea6efa3da14c7
Sreekanth N Kartha <63005945+sreekanthnkartha@users.noreply.github.com>
2022-01-22 16:40:05 +0530

    Create Crying_Colours.cpp

---
+++ Crying_Colours.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[3][3];
+	    for(int i=0;i<3;i++)
+	    for(int j=0;j<3;j++)
+	    cin>>arr[i][j];
+	    int a=arr[1][0]+arr[2][0]+arr[2][1];
+	    int b=arr[0][1]+arr[0][2]+arr[1][2];
+	    int c=(a>=b)?a:b;
+	    cout<<c<<endl;
+	}
+	return 0;
+}
