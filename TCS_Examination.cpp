cc81ebfd6e9b0a2cd281434e022239c86c5487d2
Sreekanth N Kartha <63005945+sreekanthnkartha@users.noreply.github.com>
2022-01-22 16:38:44 +0530

    Create TCS_Examination.cpp

---
+++ TCS_Examination.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int t;
+cin>>t;
+while(t--)
+{
+    int a,b,c,x,y,z;
+    cin>>a>>b>>c>>x>>y>>z;
+    int s1=a+b+c,s2=x+y+z;
+    if(s1>s2)
+    cout<<"Dragon";
+    else if(s2>s1)
+    cout<<"Sloth";
+    else if(a>x)
+    cout<<"Dragon";
+    else if(x>a)
+    cout<<"Sloth";
+    else if(b>y)
+    cout<<"Dragon";
+    else if(y>b)
+    cout<<"Sloth";
+    else
+    cout<<"Tie";
+    cout<<endl;
+    
+}
+	return 0;
+}
