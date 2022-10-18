6ba4c870868450c61cfbaa189c7fa09b209b7f5b
ajayjayaraj740 <116101802+ajayjayaraj740@users.noreply.github.com>
2022-10-18 21:53:27 +0530

    Code is optimized

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -17,8 +17,6 @@ int binarySearch(int arr[], int l, int r, int x)
 		if (arr[mid] > x)
 			return binarySearch(arr, l, mid - 1, x);
 
-		// Else the element can only be present
-		// in right subarray
 		return binarySearch(arr, mid + 1, r, x);
 	}
 
@@ -28,13 +26,16 @@ int binarySearch(int arr[], int l, int r, int x)
 
 int main(void)
 {
-	int arr[] = { 2, 3, 4, 10, 40 };
+	int arr[] = { 2, 3, 4, 10, 40, 50};
 	int x = 10;
 	int n = sizeof(arr) / sizeof(arr[0]);
 	int result = binarySearch(arr, 0, n - 1, x);
-	(result == -1)
-		? cout << "Element is not present in array"
-		: cout << "Element is present at index " << result;
+	if(result == -1){
+		cout << "Element is not present in array";
+	}
+	else{
+		cout << "Element is present at index " << result;
+	}
 	return 0;
 }
 
